8db3b7af55f32d5bd189a9739e6c5c97d6ffcb5a
Kong Yang <kpyang2414@gmail.com>
2021-07-08 21:56:35 -0500

    bases are down andready for project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
+          <h1 className="App-title">Life of a Coder</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <p>The Coding Gallery of Truth.</p>
+      <img src="images/babyarray.png" width="250" height="250"/>
       </div>
     );
 }
