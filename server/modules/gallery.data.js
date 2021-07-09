403851c8f08633d2ad066bec50be8ef17513cb32
Kong Yang <kpyang2414@gmail.com>
2021-07-08 23:11:55 -0500

    get and put is connect but not functioning, working on gallerylist next

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,7 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/babyarray.png', description: 'Honestly..why doesnt it start at one.', likes: 12 },
+    { id: 2, path: 'images/halfremember.jpeg', description: 'Secretly our best friend.', likes: 13 },
+    { id: 3, path: 'images/honestcode.png', description: 'I work hard for this.', likes: 15 },
+    { id: 4, path: 'images/morebugs.jpeg', description: 'error:error:error:error:error:error:', likes: 10 },
+    { id: 5, path: 'images/peelercode.png', description: 'To receive help please update your life to the current version.', likes: 21 },
+    { id: 6, path: 'images/simpsoncode.jpg', description: 'It never gets better, it just gets worst.', likes: 12 },
 
 ];
 
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
 
+  useEffect(() => {
+    console.log("in useEffect");
+    fetchImages();
+    updateLikes();
+  }, []);
+
   const [GalleryList, setGalleryList] = useState([]);
 
-  function fetchImages() {    
+  const fetchImages = () => {   
+    console.log('Inside GET'); 
     axios.get('/')
       .then(response => {
-        console.log('images have been retrieved'. response);
+        console.log('images have been retrieved', response);
         setGalleryList(response.data);
       })
       .catch(error => {
         console.log('images could not be retrieved', error);
       });
 
+  }
+
+  // if we are having issues connecting try both /like/:id or '/' - 
+  const updateLikes = () => {
+    console.log('Inside PUT', );
+    axios.put('/like/:id')
+    .then(response => {
+      console.log('Like is updated through PUT', response);
+    }).catch(error => {
+      console.log('Like could not be updated', error);
+    })
   }
 
     
