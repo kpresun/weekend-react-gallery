6054fca75560ac8f0c9c8dcd9ce664e00833a1c9
Kong Yang <kpyang2414@gmail.com>
2021-07-09 18:44:32 -0500

    working on app componenet

--- src/components/GalleryList/gallerylist.js
+++ src/components/GalleryList/gallerylist.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 import GalleryItem from "../GalleryItem/galleryitem";
 
 function GalleryList(props) {
-
-// I need to loop through the items with Map and Key 
-// I need something here that will grab the items information and return a jsx of:
-// image, description, likes
-    return (
-        <div>
-    {props.imgList.map(single =>
-    <GalleryItem key={single.id} imgToShow={single} />
-        </div>
-
-    )
+  // I need to loop through the items with Map and Key
+  // I need something here that will grab the items information and return a jsx of:
+  // image, description, likes
+  return (
+    <>
+      <div>
+        {props.imgList.map((single) => (
+          <GalleryItem key={single.id} imgToShow={single} />
+        ))}
+      </div>
+    </>
+  );
 }
-
 
 export default GalleryList;
