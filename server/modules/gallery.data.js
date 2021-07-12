9f0949a48fefd1340ad94a98ed5022a8a80350a7
Kong Yang <kpyang2414@gmail.com>
2021-07-11 20:24:20 -0500

    base is complete

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -4,8 +4,8 @@ const galleryItems = [
     { id: 3, path: 'images/honestcode.png', description: 'I work hard for this.', likes: 15 },
     { id: 4, path: 'images/morebugs.jpeg', description: 'error:error:error:error:error:error:', likes: 10 },
     { id: 5, path: 'images/peelercode.png', description: 'To receive help please update your life to the current version.', likes: 21 },
-    { id: 6, path: 'images/simpsoncode.jpg', description: 'It never gets better, it just gets worst.', likes: 12 },
+    { id: 6, path: 'images/simpsonscode.jpg', description: 'It never gets better, it just gets worst.', likes: 12 },
 
 ];
 
-module.exports = galleryItems;
+module.exports = galleryItems;ß
